1758e30f87239f6f8cb40b4d2c1db1c720111355
SavovicGoran <34980344+SavovicGoran@users.noreply.github.com>
2018-03-03 18:08:55 +0100

    Changes

---
+++ Gradebook/src/gradebook/Main.java
@@ -0,0 +1,30 @@
+package gradebook;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String studentName1 = "Pera";
+		String studentLastname1 = "Peric";
+		int grade1 = 8;
+		
+		String studentName2 = "Mika";
+		String studentLastname2 = "Mikic";
+		int grade2 = 9;
+		
+		String studentName3 = "Laza";
+		String studentLastname3 = "Lazic";
+		int grade3 = 8;
+		
+		String studentName4 = "Joka";
+		String studentLastname = "Jokic";
+		int grade4 = 7;
+		
+		String studentName5 = "Ana";
+		String studentLastname5 = "Anic";
+		int grade5 = 9;
+		
+		int gradeSum = grade1 + grade2 + grade3 + grade4 + grade5;
+		System.out.println("The sum of the students' grades is "+gradeSum);
+	}
+
+}
